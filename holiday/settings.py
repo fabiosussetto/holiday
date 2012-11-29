83128e76f29f0f4274267016aea9f18e65e47b34
Fabio Sussetto <fabio@bynd.com>
2012-11-29 18:17:55 +0000

    select

--- holiday/settings.py
+++ holiday/settings.py
@@ -18,6 +18,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'holiday',                      # Or path to database file if using sqlite3.
         'NAME': 'holiday_dummy',                      # Or path to database file if using sqlite3.
         #'NAME': 'holiday',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
