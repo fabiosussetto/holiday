7d30539e1a223269d23b565665c612477b9ee476
Fabio Sussetto <fabio.sussetto@gmail.com>
2012-12-04 23:55:02 +0000

    ref

---
+++ holiday_manager/static/js/calendar.js
@@ -0,0 +1,34 @@
+var HolidayRequest = Backbone.Model.extend({
+});
+
+var User = Backbone.Model.extend({
+});
+
+var UserCollection = Backbone.Collection.extend({
+    model: User
+});
+
+
+var CalendarView = Backbone.View.extend({
+    events: {
+    },
+    render: function() {
+    }
+});
+
+var UserListView = Backbone.View.extend({
+    events: {
+    },
+    render: function() {
+    }
+});
+
+var UserListItemView = Backbone.View.extend({
+    events: {
+    },
+    render: function() {
+    }
+});
+
+
+
