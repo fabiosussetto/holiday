1f47393c8d3d923dc07a06b9559958fd99325a6f
Fabio Sussetto <fabio.sussetto@gmail.com>
2012-12-12 23:12:54 +0000

    fab

--- holiday/wsgi.py
+++ holiday/wsgi.py
@@ -14,8 +14,10 @@ framework.
 
 """
 import os
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "holiday.settings")
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
